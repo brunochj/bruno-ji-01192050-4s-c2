07c9ee44b4415d04402273287fe6559495f037b9
brunochj <brunochang95@gmail.com>
2021-04-14 22:15:12 -0300

    validação se 1 for vazio

--- AvaliacaoContinuada2/app/src/main/java/com/example/avaliacaocontinuada2/TelaSucesso.kt
+++ AvaliacaoContinuada2/app/src/main/java/com/example/avaliacaocontinuada2/TelaSucesso.kt
@@ -18,24 +18,35 @@ class TelaSucesso : AppCompatActivity() {
 
         var precoTotal:Double = preco1.toString().toDouble() + preco2.toString().toDouble()
 
-        println("teste")
-        println(raca1)
-        println(raca2)
-        println(preco1)
-        println(preco2)
 
         var cachorro1: TextView = findViewById(R.id.tv_cachorro1)
         var cachorro2: TextView = findViewById(R.id.tv_cachorro2)
-
         var precoMensagem: TextView = findViewById(R.id.tv_precoTotal)
 
-        var mensagemDog1 = getString(R.string.cachorro1, raca1)
-        cachorro1.text = mensagemDog1
-        var mensagemDog2 = getString(R.string.cachorro2, raca2)
-        cachorro2.text = mensagemDog2
 
-        var mensagemPreco = getString(R.string.preco_total, precoTotal)
-        precoMensagem.text = mensagemPreco
+        if(raca1 != null || raca2 != null){
+            var mensagemDog1 = getString(R.string.cachorro1, raca1)
+            cachorro1.text = mensagemDog1
+            var mensagemDog2 = getString(R.string.cachorro2, raca2)
+            cachorro2.text = mensagemDog2
+
+            var mensagemPreco = getString(R.string.preco_total, precoTotal)
+            precoMensagem.text = mensagemPreco
+        } else if(raca1 != null && raca2 == null){
+            var mensagemDog1 = getString(R.string.cachorro1, raca1)
+            cachorro1.text = mensagemDog1
+            cachorro2.text = "Não encontrado"
+
+            var mensagemPreco = getString(R.string.preco_total, precoTotal)
+            precoMensagem.text = mensagemPreco
+        } else if(raca1 == null && raca2 != null){
+            var mensagemDog2 = getString(R.string.cachorro1, raca1)
+            cachorro2.text = mensagemDog2
+            cachorro1.text = "Não encontrado"
+
+            var mensagemPreco = getString(R.string.preco_total, precoTotal)
+            precoMensagem.text = mensagemPreco
+        }
 
 
     }
